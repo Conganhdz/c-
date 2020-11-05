028057783818fa66173a72fff559d1f91d4d7703
Hà Công Anh <62170148+Conganhdz@users.noreply.github.com>
2020-11-05 21:41:29 +0700

    Update snt.cpp

--- snt.cpp
+++ snt.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int ktsnt(int n)
 {
     int i;
-    if (n<2) return 0;
+    if (n<2){
+        return 0;
+    }
     for (i=2; i<=sqrt(n); i++){
         if (n%i==0)
         return 0;
